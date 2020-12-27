184cb9ab400d67135b2d7bf2968daa902e1606eb
Den9 <den9.rcq@gmail.com>
2020-12-28 00:04:01 +0300

    added modal window & js script & adaptive

--- src/js/script.js
+++ src/js/script.js
@@ -20,15 +20,14 @@ $(document).ready(function () {
 
 
     // Tabs ======================================================================================
-
     $('ul.catalog__tabs').on('click', 'li:not(.catalog__tab_active)', function () {
         $(this)
             .addClass('catalog__tab_active').siblings().removeClass('catalog__tab_active')
             .closest('div.container').find('div.catalog__row').removeClass('catalog__row_active').eq($(this).index()).addClass('catalog__row_active');
     });
 
-    // Slide =====================================================================================
 
+    // Slide =====================================================================================
     function toggleSlide(item) {
         $(item).each(function (i) {
             $(this).on('click', function (e) {
@@ -38,8 +37,28 @@ $(document).ready(function () {
             })
         })
     };
-
     toggleSlide('.catalog__link');
     toggleSlide('.catalog__link-back');
+
+
+    // Modal window ==============================================================================
+
+    // Show modal window
+    $('[data-modal=consultation]').on('click', function () {
+        $('.overlay, #consultation').fadeIn('slow');
+    });
+
+    // Show modal window and rename .catalog__product-name
+    $('.button_mini').each(function (i) {
+        $(this).on('click', function () {
+            $('#order .modal__descr').text($('.catalog__product-name').eq(i).text());
+            $('.overlay, #order').fadeIn('slow');
+        });
+    });
+
+    // Hide modal window
+    $('.modal__close').on('click', function () {
+        $('.overlay, #consultation, #thanks, #order').fadeOut('slow');
+    });
 
 });
